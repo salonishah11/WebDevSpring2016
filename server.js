3441335a2e1ace7f21ad98ccba70d1dcbd653760
salonishah11 <shah.salo@husky.neu.edu>
2016-01-17 16:06:59 -0500

    Edited server.js Added '/hello' in app.get function

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-app.get('/', function(req, res){
+app.get('/hello', function(req, res){
     res.send('hello world');
 });
 app.listen(port, ipaddress);

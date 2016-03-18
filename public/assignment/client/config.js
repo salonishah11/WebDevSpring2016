eeac41cee6c572be4ff7aff10b0e6350b23f8fe5
salonishah11 <salonipshah11@gmail.com>
2016-03-17 23:22:53 -0400

    Created proper directories

--- public/assignment/client/config.js
+++ public/assignment/client/config.js
@@ -7,29 +7,29 @@
     function Configure($routeProvider) {
         $routeProvider
             .when("/home",{
-                templateUrl: "../assignment/views/home/home.view.html"
+                templateUrl: "views/home/home.view.html"
             })
             .when("/register",{
-                templateUrl: "../assignment/views/users/register.view.html",
+                templateUrl: "views/users/register.view.html",
                 controller: "RegisterController"
             })
             .when("/login",{
-                templateUrl: "../assignment/views/users/login.view.html",
+                templateUrl: "views/users/login.view.html",
                 controller: "LoginController"
             })
             .when("/profile",{
-                templateUrl: "../assignment/views/users/profile.view.html",
+                templateUrl: "views/users/profile.view.html",
                 controller: "ProfileController"
             })
             .when("/admin",{
-                templateUrl: "../assignment/views/admin/admin.view.html"
+                templateUrl: "views/admin/admin.view.html"
             })
             .when("/forms",{
-                templateUrl: "../assignment/views/forms/forms.view.html",
+                templateUrl: "views/forms/forms.view.html",
                 controller: "FormController"
             })
             .when("/fields",{
-                templateUrl: "../assignment/views/forms/fields.view.html"
+                templateUrl: "views/forms/fields.view.html"
             })
             .otherwise({
                 redirectTo: "/home"
